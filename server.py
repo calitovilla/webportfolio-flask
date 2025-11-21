c2bd4444ef834a95b985bbef773051bf78d082f3
calitovilla <calitovilla@users.noreply.github.com>
2025-11-21 11:41:23 +0100

    refactoring

--- server.py
+++ server.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/works")
+def works():
+    return render_template("works.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
