a99d51339bc0cc550e30fdcdc24727e614d1e8b9
calitovilla <calitovilla@users.noreply.github.com>
2025-11-21 13:53:03 +0100

    thank you response

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from jinja2 import TemplateNotFound
 
 app = Flask(__name__)
+app.secret_key = 'tu_clave_secreta' 
 
 @app.route("/")
 def home():
@@ -13,3 +14,22 @@ def page(subpath):
         return render_template(f"{subpath}.html")
     except TemplateNotFound:
         return "Page not found", 404
+
+@app.route('/thankyou')
+def thankyou():
+    email = request.args.get('email')
+    return render_template('thankyou.html', email=email)
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    error = None
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            email = data.get('email')
+            return redirect(url_for('thankyou', email=email))
+        except Exception as e:
+            error = str(e)
+            return f"An error occurred: {error}"
+    else:
+        return "Something went wrong. Try again!"
